1a7d305eda63d8541eb76617fe079f215b71b6fa
Raffi Dilanchian <raffidilanchian@gmail.com>
2018-03-27 13:06:00 +0430

    edit hooks

--- src/main.ino
+++ src/main.ino
@@ -43,28 +43,11 @@ bool powerOnHandler(const HomieRange& range, const String& value) {
 
 void onHomieEvent(const HomieEvent& event) {
   switch(event.type) {
-    case HomieEventType::STANDALONE_MODE:
-      // Do whatever you want when standalone mode is started
-      break;
     case HomieEventType::CONFIGURATION_MODE:
       // Do whatever you want when configuration mode is started
       break;
     case HomieEventType::NORMAL_MODE:
       // Do whatever you want when normal mode is started
-      break;
-    case HomieEventType::OTA_STARTED:
-      // Do whatever you want when OTA is started
-      break;
-    case HomieEventType::OTA_PROGRESS:
-      // Do whatever you want when OTA is in progress
-
-      // You can use event.sizeDone and event.sizeTotal
-      break;
-    case HomieEventType::OTA_FAILED:
-      // Do whatever you want when OTA is failed
-      break;
-    case HomieEventType::OTA_SUCCESSFUL:
-      // Do whatever you want when OTA is successful
       break;
     case HomieEventType::ABOUT_TO_RESET:
       // Do whatever you want when the device is about to reset
@@ -86,14 +69,6 @@ void onHomieEvent(const HomieEvent& event) {
       // Do whatever you want when MQTT is disconnected in normal mode
 
       // You can use event.mqttReason
-      break;
-    case HomieEventType::MQTT_PACKET_ACKNOWLEDGED:
-      // Do whatever you want when an MQTT packet with QoS > 0 is acknowledged by the broker
-
-      // You can use event.packetId
-      break;
-    case HomieEventType::READY_TO_SLEEP:
-      // After you've called `prepareToSleep()`, the event is triggered when MQTT is disconnected
       break;
   }
 }
