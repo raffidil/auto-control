f4fedcccc2d9f39e67c206625f63b2bc6b12ce76
Samim Pezeshki <p.samim@gmail.com>
2018-04-03 23:47:56 +0430

    Update room

--- src/main.ino
+++ src/main.ino
@@ -4,11 +4,13 @@
 
 #define PIN_KEY1 D3
 #define PIN_KEY2 D1
-//#define PIN_KEY3 D7
+#define PIN_KEY3 D7
+
+long buttonDebounce = 200;
 
 const int PIN_RELAY1 = D5;
 const int PIN_RELAY2 = D2;
-// const int PIN_RELAY3 = D4;
+const int PIN_RELAY3 = D4;
 
 const int PIN_LED = D6;
 
@@ -20,13 +22,14 @@ bool lastPinValue2 = HIGH;
 bool relayState2 = HIGH;
 bool currentValue2;
 
-// bool lastPinValue3 = HIGH;
-// bool relayState3 = HIGH;
-// bool currentValue3;
+bool lastPinValue3 = HIGH;
+bool relayState3 = HIGH;
+bool currentValue3;
+long long buttonTime3 = 0;
 
 HomieNode relayNode1("relay1", "relay");
 HomieNode relayNode2("relay2", "relay");
-// HomieNode relayNode3("relay3", "relay");
+HomieNode relayNode3("relay3", "relay");
 
 void buttonLoop();
 void homieLoop();
@@ -45,7 +48,7 @@ void ledColor(uint8_t r, uint8_t g, uint8_t b) {
 void buttonLoop() {
   currentValue1 = digitalRead(PIN_KEY1);
   currentValue2 = digitalRead(PIN_KEY2);
-  // currentValue3 = digitalRead(PIN_KEY3);
+  currentValue3 = digitalRead(PIN_KEY3);
 
   if (currentValue1 != lastPinValue1) {
     relayState1 = !relayState1;
@@ -67,18 +70,21 @@ void buttonLoop() {
     }
   }
 
-  // if (currentValue3 != lastPinValue3) {
-  //   relayState3 = !relayState3;
-  //    digitalWrite(PIN_RELAY3, relayState3);
-  //    Serial.print("relay3: ");
-  //    Serial.println(relayState3 ? "OFF" : "ON");
-  //    if(Homie.isConfigured() && Homie.isConnected()){
-  //      relayNode3.setProperty("power").send(relayState3 ? "OFF" : "ON");
-  //    }
-  // }
+  if (currentValue3 == HIGH && lastPinValue3 == LOW &&
+      millis() - buttonTime3 > buttonDebounce) {
+    relayState3 = !relayState3;
+    digitalWrite(PIN_RELAY_3, relaySate3);
+    Serial.print("relay3 is: ");
+    Serial.println(relayState3 ? "OFF" : "ON");
+    if (Homie.isConfigured() && Homie.isConnected()) {
+      relayNode3.setProperty("power").send(relayState3 ? "OFF" : "ON");
+    }
+    buttonTime3 = millis();
+  }
+
   lastPinValue1 = currentValue1;
   lastPinValue2 = currentValue2;
-  // lastPinValue3 = currentValue3;
+  lastPinValue3 = currentValue3;
 }
 
 void setup() {
@@ -89,27 +95,24 @@ void setup() {
   // relay and button
   pinMode(PIN_RELAY1, OUTPUT);
   pinMode(PIN_RELAY2, OUTPUT);
-  // pinMode(PIN_RELAY3, OUTPUT);
+  pinMode(PIN_RELAY3, OUTPUT);
 
   pinMode(PIN_KEY1, INPUT_PULLUP);
   pinMode(PIN_KEY2, INPUT_PULLUP);
-  // pinMode(PIN_KEY3, INPUT_PULLUP);
+  pinMode(PIN_KEY3, INPUT_PULLUP);
 
   pinMode(PIN_LED, OUTPUT);
 
   digitalWrite(PIN_RELAY1, relayState1);
   digitalWrite(PIN_RELAY2, relayState2);
-  // digitalWrite(PIN_RELAY3, relayState3);
-
-  // lastPinValue1 = digitalRead(PIN_KEY1);
-  // lastPinValue2 = digitalRead(PIN_KEY2);
+  digitalWrite(PIN_RELAY3, relayState3);
 
   Serial.print("lastPinValue1: ");
   Serial.println(lastPinValue1);
   Serial.print("lastPinValue2: ");
   Serial.println(lastPinValue2);
-  // Serial.print("lastPinValue3: ");
-  // Serial.println(lastPinValue3);
+  Serial.print("lastPinValue3: ");
+  Serial.println(lastPinValue3);
 
   runner.init();
   runner.addTask(buttonTask);
